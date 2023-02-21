3c168532b95cd065d2aeea91f9af91c3f4a61584
yevgeny <yevgeny.shtraiher@highcon.net>
2023-02-21 16:45:51 +0200

    deleted unneccecery stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MyCustomDirectiveDirective } from './my-custom-directive.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyCustomDirectiveDirective
+    MyCustomDirectiveDirective,
+    TimesDirective
   ],
   imports: [
     BrowserModule
